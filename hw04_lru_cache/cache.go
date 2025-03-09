c073febe44feeb086cf37561b0a0f7b0258c5b13
NTimakov <ntimakov@mail.ru>
2025-03-09 11:00:25 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -23,3 +23,28 @@ func NewCache(capacity int) Cache {
 		items:    make(map[Key]*ListItem, capacity),
 	}
 }
+
+func (cache lruCache) Get(key Key) (interface{}, bool) {
+	if listItem, ok := cache.items[key]; ok {
+		cache.queue.MoveToFront(listItem)
+
+		return listItem.Value, true
+	}
+
+	return nil, false
+}
+
+func (cache lruCache) Set(key Key, value interface{}) bool {
+	if listItem, ok := cache.items[key]; ok {
+		listItem.Value = value
+
+		return true
+	}
+
+	return false
+}
+
+func (cache lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items = make(map[Key]*ListItem, cache.capacity)
+}
